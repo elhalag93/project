fe126ca42fa440d8fcba745be483e2f40e5cd711
MohabOmar <mohabomar15@gmail.com>
2020-01-22 17:04:25 +0200

    reset

--- gemy_java_script.js
+++
@@ -1,8 +0,0 @@
-// document.getElementById("gemy_div").style.margin = "150px 150px 150px 150px";
-// document.getElementById("gemy_div").style.margin = "auto";
-// document.getElementById("first").style.background = "yellow";
-// document.getElementById("second").style.backgroundColor = "green";
-// document.getElementById("gemy_div_1").style.margin = "auto";
-// document.getElementById("gemy_div_1").style.margin = "auto";
-
-
